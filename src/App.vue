eb4124063fec25eaeb162877089f39a08579916b
SakuraLoveArcaea <68677116+ToBy-RedStones@users.noreply.github.com>
2026-01-07 22:56:58 +0800

    Initial setup for gh-pages

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MaimaiSensor from "./maimaiSensor/MaimaiSensor.vue";
+</script>
+
+<template>
+  <MaimaiSensor></MaimaiSensor>
+</template>
+
+<style>
+body {
+    /* 背景黑色 */
+    background-color: #000000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100dvh;
+    margin: 0;
+    overflow: hidden; /* 防止滾動條出現 */
+}
+</style>
